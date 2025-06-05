1ae62808ceb1fb80238b4e7cb0cbdd63a2c94303
Fidel Imasuen <fetinosa@gmail.com>
2025-06-05 10:01:55 +0100

    Update api.py

--- api.py
+++ api.py
@@ -3,8 +3,8 @@ import os
 from skimage.io import imread, imsave
 from skimage.transform import estimate_transform, warp
 from time import time
-
-from predictor import PosPrediction
+from skimage.transform import warp, resize, estimate_transform
+from PRNet.predictor import PosPrediction
 
 class PRN:
     ''' Joint 3D Face Reconstruction and Dense Alignment with Position Map Regression Network
@@ -19,11 +19,11 @@ class PRN:
         self.resolution_op = 256
 
         #---- load detectors
-        if is_dlib:
-            import dlib
-            detector_path = os.path.join(prefix, 'Data/net-data/mmod_human_face_detector.dat')
-            self.face_detector = dlib.cnn_face_detection_model_v1(
-                    detector_path)
+        #if is_dlib:
+            #import dlib
+            #detector_path = os.path.join(prefix, 'Data/net-data/mmod_human_face_detector.dat')
+          #  self.face_detector = dlib.cnn_face_detection_model_v1(
+                    #detector_path)
 
         #---- load PRN 
         self.pos_predictor = PosPrediction(self.resolution_inp, self.resolution_op)
@@ -31,7 +31,7 @@ class PRN:
         if not os.path.isfile(prn_path + '.data-00000-of-00001'):
             print("please download PRN trained model first.")
             exit()
-        self.pos_predictor.restore(prn_path)
+        #self.pos_predictor.restore(prn_path)
 
         # uv file
         self.uv_kpt_ind = np.loadtxt(prefix + '/Data/uv-data/uv_kpt_ind.txt').astype(np.int32) # 2 x 68 get kpt
@@ -49,8 +49,8 @@ class PRN:
         uv_coords = np.hstack((uv_coords[:,:2], np.zeros([uv_coords.shape[0], 1])))
         return uv_coords
 
-    def dlib_detect(self, image):
-        return self.face_detector(image, 1)
+    #def dlib_detect(self, image):
+        #return self.face_detector(image, 1)
 
     def net_forward(self, image):
         ''' The core of out method: regress the position map of a given image.
@@ -59,76 +59,99 @@ class PRN:
         Returns:
             pos: the 3D position map. (256, 256, 3) array.
         '''
-        return self.pos_predictor.predict(image)
+        return self.pos_predictor(image)
 
-    def process(self, input, image_info = None):
-        ''' process image with crop operation.
-        Args:
-            input: (h,w,3) array or str(image path). image value range:1~255. 
-            image_info(optional): the bounding box information of faces. if None, will use dlib to detect face. 
+    def process(self, input, image_info=True):
+              """
+              process image with crop operation.
+              Args:
+                  input: (h,w,3) array or str(image path). image value range: 1~255.
+                  image_info (optional): if not None, indicates that the input image is already
+                      the cropped face (bounding box == full image). In that case, we skip the
+                      usual similarity transform and just resize to resolution_inp.
+              Returns:
+                  pos: the 3D position map. (resolution_op, resolution_op, 3).
+              """
+              if isinstance(input, str):
+                  try:
+                      image = imread(input)
+                  except IOError:
+                      print("error opening file:", input)
+                      return None
+              else:
+                  image = input
 
-        Returns:
-            pos: the 3D position map. (256, 256, 3).
-        '''
-        if isinstance(input, str):
-            try:
-                image = imread(input)
-            except IOError:
-                print("error opening file: ", input)
-                return None
-        else:
-            image = input
+              if image.ndim < 3:
+                  image = np.tile(image[:, :, np.newaxis], [1, 1, 3])
 
-        if image.ndim < 3:
-            image = np.tile(image[:,:,np.newaxis], [1,1,3])
+              if True:
+                  # Input is exactly the bounding‐box crop → no need to compute src_pts or warp
+                  # Just normalize and resize straight to resolution_inp
+                  image_norm = image.astype(np.float32) / 255.0
+                  cropped_image = resize(
+                      image_norm,
+                      (self.resolution_inp, self.resolution_inp),
+                      mode='reflect',
+                      anti_aliasing=True
+                  )
+              else:
+                  # Original behavior: detect face via dlib and perform similarity‐based crop
+                  detected_faces = self.dlib_detect(image)
+                  if len(detected_faces) == 0:
+                      print('warning: no detected face')
+                      return None
 
-        if image_info is not None:
-            if np.max(image_info.shape) > 4: # key points to get bounding box
-                kpt = image_info
-                if kpt.shape[0] > 3:
-                    kpt = kpt.T
-                left = np.min(kpt[0, :]); right = np.max(kpt[0, :]); 
-                top = np.min(kpt[1,:]); bottom = np.max(kpt[1,:])
-            else:  # bounding box
-                bbox = image_info
-                left = bbox[0]; right = bbox[1]; top = bbox[2]; bottom = bbox[3]
-            old_size = (right - left + bottom - top)/2
-            center = np.array([right - (right - left) / 2.0, bottom - (bottom - top) / 2.0])
-            size = int(old_size*1.6)
-        else:
-            detected_faces = self.dlib_detect(image)
-            if len(detected_faces) == 0:
-                print('warning: no detected face')
-                return None
+                  d = detected_faces[0].rect  # assume one face
+                  left, right = d.left(), d.right()
+                  top, bottom = d.top(), d.bottom()
+                  old_size = (right - left + bottom - top) / 2
+                  center = np.array([
+                      right - (right - left) / 2.0,
+                      bottom - (bottom - top) / 2.0 + old_size * 0.14
+                  ])
+                  size = int(old_size * 1.58)
 
-            d = detected_faces[0].rect ## only use the first detected face (assume that each input image only contains one face)
-            left = d.left(); right = d.right(); top = d.top(); bottom = d.bottom()
-            old_size = (right - left + bottom - top)/2
-            center = np.array([right - (right - left) / 2.0, bottom - (bottom - top) / 2.0 + old_size*0.14])
-            size = int(old_size*1.58)
+                  # define source and destination points for similarity transform
+                  src_pts = np.array([
+                      [center[0] - size / 2, center[1] - size / 2],
+                      [center[0] - size / 2, center[1] + size / 2],
+                      [center[0] + size / 2, center[1] - size / 2]
+                  ])
+                  DST_PTS = np.array([
+                      [0, 0],
+                      [0, self.resolution_inp - 1],
+                      [self.resolution_inp - 1, 0]
+                  ])
+                  tform = estimate_transform('similarity', src_pts, DST_PTS)
 
-        # crop image
-        src_pts = np.array([[center[0]-size/2, center[1]-size/2], [center[0] - size/2, center[1]+size/2], [center[0]+size/2, center[1]-size/2]])
-        DST_PTS = np.array([[0,0], [0,self.resolution_inp - 1], [self.resolution_inp - 1, 0]])
-        tform = estimate_transform('similarity', src_pts, DST_PTS)
-        
-        image = image/255.
-        cropped_image = warp(image, tform.inverse, output_shape=(self.resolution_inp, self.resolution_inp))
+                  image_norm = image.astype(np.float32) / 255.0
+                  cropped_image = warp(
+                      image_norm,
+                      tform.inverse,
+                      output_shape=(self.resolution_inp, self.resolution_inp)
+                  )
 
-        # run our net
-        #st = time()
-        cropped_pos = self.net_forward(cropped_image)
-        #print 'net time:', time() - st
+              # run the network
+              cropped_pos = self.net_forward(cropped_image)
 
-        # restore 
-        cropped_vertices = np.reshape(cropped_pos, [-1, 3]).T
-        z = cropped_vertices[2,:].copy()/tform.params[0,0]
-        cropped_vertices[2,:] = 1
-        vertices = np.dot(np.linalg.inv(tform.params), cropped_vertices)
-        vertices = np.vstack((vertices[:2,:], z))
-        pos = np.reshape(vertices.T, [self.resolution_op, self.resolution_op, 3])
-        
-        return pos
+              # restore to original coordinate space (only needed if image_info was None)
+              if image_info is None:
+                  cropped_vertices = np.reshape(cropped_pos, [-1, 3]).T
+                  z = cropped_vertices[2, :].copy() / tform.params[0, 0]
+                  cropped_vertices[2, :] = 1
+                  vertices = np.dot(np.linalg.inv(tform.params), cropped_vertices)
+                  vertices = np.vstack((vertices[:2, :], z))
+                  pos = np.reshape(vertices.T, [self.resolution_op, self.resolution_op, 3])
+              else:
+                  # If we skipped cropping, just upsample/downsample network output
+                  pos = resize(
+                      cropped_pos,
+                      (self.resolution_op, self.resolution_op, 3),
+                      mode='reflect',
+                      anti_aliasing=True
+                  ).astype(np.float32)
+
+              return pos
             
     def get_landmarks(self, pos):
         '''
